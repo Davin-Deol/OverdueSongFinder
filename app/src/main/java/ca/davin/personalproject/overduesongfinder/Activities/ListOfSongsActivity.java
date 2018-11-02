b3b9253e74629ebd294389f281ad360c79be7ac0
Davin Deol <davindeol@gmail.com>
2018-11-02 11:57:27 -0700

    Added App Bar

--- app/src/main/java/ca/davin/personalproject/overduesongfinder/Activities/ListOfSongsActivity.java
+++ app/src/main/java/ca/davin/personalproject/overduesongfinder/Activities/ListOfSongsActivity.java
@@ -2,6 +2,11 @@ package ca.davin.personalproject.overduesongfinder.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import ca.davin.personalproject.overduesongfinder.R;
 
@@ -11,5 +16,30 @@ public class ListOfSongsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_songs);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                Toast.makeText(this, "Info Pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_findSongs:
+                Toast.makeText(this, "Find Songs Pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_purchase:
+                Toast.makeText(this, "Purchase Pressed", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list_of_songs, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
